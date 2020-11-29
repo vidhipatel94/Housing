b085e88fa51bd9b9a67be4da491f263c9df39adf
Vidhi Patel <vidhibpatel94@gmail.com>
2020-11-29 14:39:11 +0530

    Empty Dashboard and Register view controller added

---
+++ Housing/DashboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DashboardViewController.swift
+//  Housing
+//
+//  Created by Vidhi Patel on 29/11/20.
+//  Copyright © 2020 Conestoga. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
